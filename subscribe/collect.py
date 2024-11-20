39684555e449fcc632b423dde80390d27027a870
Alan Umir <30733960+ltsj@users.noreply.github.com>
2024-11-21 04:32:59 +0800

    Update collect.py

--- subscribe/collect.py
+++ subscribe/collect.py
@@ -563,7 +563,7 @@ if __name__ == "__main__":
         "--targets",
         nargs="+",
         choices=subconverter.CONVERT_TARGETS,
-        default=["clash", "v2ray", "singbox"],
+        default=["clash", "v2ray", "ssr", "singbox"],
         help=f"choose one or more generated profile type. default to clash, v2ray and singbox. supported: {subconverter.CONVERT_TARGETS}",
     )
 
